e77bb81afac32377c9a935c5ec18d9d1380396b4
BeauTheBeau <beauthebeau@proton.me>
2023-11-16 19:09:24 +0000

    Smarter defaults based on terminal size
    Script now calculates default size based on image aspect ratio and terminal size.

--- gang-signs.sh
+++ gang-signs.sh
@@ -7,7 +7,7 @@ Help()
     echo "Syntax: gang-signs.sh [-w width] [-d delay] [-h]"
     echo "Options:"
     echo "  -h    help     Show this help message."
-    echo "  -w    width    The width of the picture in characters. Defaults to the terminal width, capped at 360."
+    echo "  -w    width    The width of the picture in characters. Default is calculated based on terminal size."
     echo "  -d    delay    The delay between frames in seconds. Defaults to 0.1."
 }
 
@@ -15,6 +15,23 @@ Help()
 width=$(tput cols)
 height=$(tput lines)
 delay=0.1
+
+target_width=360
+target_height=202
+
+# Calculate adjusted dimensions to maintain aspect ratio
+adjusted_width=$(echo "scale=0; ($height * $target_width) / $target_height" | bc)
+adjusted_height=$(echo "scale=0; ($width * $target_height) / $target_width" | bc)
+
+# Get new dimensions based on aspect ratio
+if [ "$adjusted_width" -gt "$width" ]; then
+    width=$width
+    height=$adjusted_height
+else
+    width=$adjusted_width
+    height=$height
+fi
+
 
 # Parse command line arguments
 while getopts ":w:d:h:" opt; do
